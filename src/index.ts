4c3f94f6651c971d96a0cf69fe78fdf986c915cc
schettn <52858351+schettn@users.noreply.github.com>
2022-09-07 16:27:13 +0000

    fix: export signup

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-export {default as jsonFetch} from './jsonFetch.js'
-export {default as login} from './login.js'
-export {default as logout} from './logout.js'
+export { default as jsonFetch } from './jsonFetch.js'
+export { default as login } from './login.js'
+export { default as logout } from './logout.js'
+export { default as signup } from './signup.js'
+
