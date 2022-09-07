d41b07c0a8309bc0592f35083aafa8c0a2464a00
Nico Schett <52858351+schettn@users.noreply.github.com>
2022-09-07 18:19:22 +0000

    fix: set userData cookie for origin

--- src/helper/user.ts
+++ src/helper/user.ts
@@ -1,14 +1,15 @@
-import {Response} from 'express'
+import type {Request, Response} from 'express'
 
 import {
   COOKIE_OPTIONS,
-  USER_DATA_TOKEN_NAME,
-  LOGIN_TOKEN_COOKIE_MAX_AGE
+  LOGIN_TOKEN_COOKIE_MAX_AGE,
+  USER_DATA_TOKEN_NAME
 } from '../constants.js'
 
-export function setUserCookie(res: Response, userDataToken) {
+export function setUserCookie(req: Request, res: Response, userDataToken: any) {
   res.cookie(USER_DATA_TOKEN_NAME, userDataToken, {
     ...COOKIE_OPTIONS,
+    domain: req.headers.origin,
     maxAge: LOGIN_TOKEN_COOKIE_MAX_AGE * 1000,
     httpOnly: false
   })
