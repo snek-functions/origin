b06b4f5b3941f34c2266e4a7a4cd4f15a7be6989
Pinterics David <55298934+pinterid@users.noreply.github.com>
2022-09-07 11:52:28 +0000

    feat: add userData cookie

--- src/logout.ts
+++ src/logout.ts
@@ -3,7 +3,8 @@ import {fn} from './factory'
 import {
   COOKIE_OPTIONS,
   REFRESH_TOKEN_COOKIE_NAME,
-  TOKEN_COOKIE_NAME
+  TOKEN_COOKIE_NAME,
+  USER_DATA_TOKEN_NAME
 } from './constants.js'
 import loginRequired from './decorators/loginRequired.js'
 
@@ -11,6 +12,7 @@ const logout = fn<{username: string; password: string}, void>(
   async (args, _, {res}) => {
     res.clearCookie(TOKEN_COOKIE_NAME, COOKIE_OPTIONS)
     res.clearCookie(REFRESH_TOKEN_COOKIE_NAME, COOKIE_OPTIONS)
+    res.clearCookie(USER_DATA_TOKEN_NAME, COOKIE_OPTIONS)
   },
   {
     name: 'logout',
