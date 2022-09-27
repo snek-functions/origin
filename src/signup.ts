982091718cbaf45886c3d289372baf5284366896
Florian Kleber <kleberbaum@erebos.xyz>
2022-09-27 09:17:25 +0000

    feat: implement send2fa
     - Remove signupEmail

--- src/signup.ts
+++ src/signup.ts
@@ -1,48 +1,44 @@
-import {send} from '@snek-functions/email'
-import {signupEmail} from './email/signupEmail'
-import {fn, url} from './factory'
+import { send2fa } from "@snek-functions/email";
+import { fn, url } from "./factory";
 
 const signup = fn<
   {
-    email: string
-    password: string
-    details: {firstName: string; lastName: string}
+    email: string;
+    password: string;
+    details: { firstName: string; lastName: string };
   },
   void
 >(
-  async (args, _, {res}) => {
-    const {newAccessToken} = await import('./internal/token/factory.js')
+  async (args, _, { res }) => {
+    const { newAccessToken } = await import("./internal/token/factory.js");
     const scope = {
-      res1: ['read', 'write'],
-      res2: ['read', 'write']
-    }
+      res1: ["read", "write"],
+      res2: ["read", "write"],
+    };
 
-    const {accessToken} = newAccessToken({
-      subject: '0',
+    const { accessToken } = newAccessToken({
+      subject: "0",
       data: args,
       scope,
-      durration: '30d'
-    })
+      durration: "30d",
+    });
 
-    const emailRes = await send.execute({
+    const emailRes = await send2fa.execute({
       email: args.email,
-      subject: 'Confirm your Registration at PhotonQ',
-      msg: signupEmail(
-        url,
-        accessToken,
-        args.details.firstName,
-        args.details.lastName
-      )
-    })
+      subject: "Confirm your Registration at PhotonQ",
+      link: `${url.replace("/graphql", "/submit")}?token=${accessToken}`,
+      firstName: args.details.firstName,
+      lastName: args.details.lastName,
+    });
 
     if (emailRes.errors.length > 0) {
-      throw new Error(emailRes.errors[0].message)
+      throw new Error(emailRes.errors[0].message);
     }
   },
   {
-    name: 'signup',
-    decorators: []
+    name: "signup",
+    decorators: [],
   }
-)
+);
 
-export default signup
+export default signup;
