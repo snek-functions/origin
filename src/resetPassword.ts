f2cd69b305d596d1adf23dd95ade65a9b63b29ea
schettn <52858351+schettn@users.noreply.github.com>
2022-09-30 13:52:13 +0000

    feat: move reset password from app to resetPassword

--- src/resetPassword.ts
+++ src/resetPassword.ts
@@ -1,48 +1,78 @@
 import {send} from '@snek-functions/email'
+import {usersUpdate} from '@snek-functions/iam'
 
 import {fn, url} from './factory'
+import {setAuthentication} from './helper/auth'
 
+// Maybe this should be split into two functions
 const resetPassword = fn<
-  {
-    email: string
-  },
+  | {
+      email: string
+    }
+  | {
+      token: string
+      password: string
+    },
   void
 >(
-  async (args, _, {req, res}) => {
-    const {newToken} = await import('./internal/token/factory.js')
+  async (
+    args: {token?: string; email?: string; password?: string},
+    _,
+    {req, res}
+  ) => {
+    const {newToken, verify} = await import('./internal/token/factory.js')
 
     const {userGet} = await import('@snek-functions/iam')
 
-    const user = await userGet({alias: args.email})
+    if (args.token) {
+      const {sub, type} = verify(args.token)
 
-    const {token} = newToken(
-      {
-        subject: user.userId,
-        payload: {
-          data: {
-            email: args.email
+      if (type === 'password_reset') {
+        if (!sub) {
+          throw new Error('No user id provided')
+        }
+
+        await usersUpdate({
+          userId: sub,
+          password: args.password
+        })
+
+        setAuthentication(sub, res)
+      }
+    }
+
+    if (args.email) {
+      const user = await userGet({alias: args.email})
+
+      const {token} = newToken(
+        {
+          subject: user.userId,
+          payload: {
+            data: {
+              email: args.email
+            },
+            scope: null
           },
-          scope: null
+
+          durration: '5 minutes'
         },
+        'password_reset'
+      )
 
-        durration: '5 minutes'
-      },
-      'password_reset'
-    )
+      console.log('token', token)
 
-    console.log('token', token)
-
-    await send({
-      email: args.email,
-      subject: 'Reset your Password at PhotonQ',
-      msg: `
-        <p>Click the link below to reset your password</p>
-        <a href="${url.replace(
-          '/graphql',
-          '/reset'
-        )}?token=${token}">Reset Password</a>
-`
-    })
+      await send({
+        email: args.email,
+        subject: 'Reset your Password at PhotonQ',
+        msg: `
+          <p>Click the link below to reset your password</p>
+          <a href="${url.replace(
+            '/graphql',
+            '/reset'
+          )}?token=${token}">Reset Password</a>
+  `
+      })
+    }
   },
   {
     name: 'resetPassword',
