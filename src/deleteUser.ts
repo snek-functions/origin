c4b44f094d5ebc54a78e6869ea1e55eabb6625b5
Florian Kleber <kleberbaum@erebos.xyz>
2022-10-24 22:45:12 +0000

    feat: add deleteUser

---
+++ src/deleteUser.ts
@@ -0,0 +1,79 @@
+import { sendPasswordReset } from '@snek-functions/email'
+
+import { fn, url } from './factory'
+
+// Maybe this should be split into two functions
+const deleteUser = fn<
+  | {
+    email: string
+  }
+  | {
+    token: string
+    password: string
+  },
+  void
+>(
+  async (
+    args: { token?: string; email?: string; password?: string },
+    _,
+    { req, res }
+  ) => {
+    const { newToken, verify } = await import('./internal/token/factory.js')
+    const { setAuthentication } = await import('./helper/auth.js')
+
+    const { userGet } = await import('@snek-functions/iam')
+
+    if (args.token) {
+      const { sub, type } = verify(args.token)
+
+      if (type === 'password_reset') {
+        const { usersUpdate } = await import('@snek-functions/iam')
+        if (!sub) {
+          throw new Error('No user id provided')
+        }
+
+        await usersUpdate({
+          userId: sub,
+          password: args.password
+        })
+
+        setAuthentication(sub, res)
+      }
+    }
+
+    if (args.email) {
+      const user = await userGet({ alias: args.email })
+
+      const { token } = newToken(
+        {
+          subject: user.userId,
+          payload: {
+            data: {
+              email: args.email
+            },
+            scope: null
+          },
+
+          durration: '5 minutes'
+        },
+        'password_reset'
+      )
+
+      console.log('token', token)
+
+      await sendPasswordReset({
+        email: args.email,
+        subject: 'Delete user at PhotonQ',
+        link: `${url.replace('/graphql', '/delete')}?token=${token}`,
+        firstName: user.firstName,
+        lastName: user.lastName
+      })
+    }
+  },
+  {
+    name: 'deleteUser',
+    decorators: []
+  }
+)
+
+export default deleteUser
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-export {default as jsonFetch} from './jsonFetch.js'
-export {default as login} from './login.js'
-export {default as logout} from './logout.js'
-export {default as resetPassword} from './resetPassword.js'
-export {default as signup} from './signup.js'
+export { default as deleteUser } from './deleteUser.js'
+export { default as jsonFetch } from './jsonFetch.js'
+export { default as login } from './login.js'
+export { default as logout } from './logout.js'
+export { default as resetPassword } from './resetPassword.js'
+export { default as signup } from './signup.js'
 
 // SPDX-License-Identifier: (EUPL-1.2)
 // Copyright © 2019-2022 snek.at
