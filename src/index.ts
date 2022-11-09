bc1b9dcb78b82fa34183e3631914edf51925ae1e
Nico Schett <52858351+schettn@users.noreply.github.com>
2022-11-09 22:02:30 +0000

    fix: export shopify service

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,12 @@ export {default as requestForInformation} from './requestForInformation.js'
 export {default as resetPassword} from './resetPassword.js'
 export {default as signup} from './signup.js'
 export {default as updateAccount} from './updateAccount.js'
+export {default as shopifyAllProductId} from './shopifyAllProductId.js'
+export {default as shopifyCreateProduct} from './shopifyCreateProduct.js'
+export {default as shopifyDeleteProduct} from './shopifyDeleteProduct.js'
+export {default as shopifyUpdateProduct} from './shopifyUpdateProduct.js'
+export {default as shopifyValidateSecret} from './shopifyValidateSecret.js'
+
 
 // SPDX-License-Identifier: (EUPL-1.2)
 // Copyright © 2019-2022 snek.at
